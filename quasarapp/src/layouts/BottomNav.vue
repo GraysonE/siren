908e1c96ce2a504f293e8a48664baef5ffdf5637
Grayson Erhard <web@graysonerhard.com>
2020-10-06 15:12:07 -0600

    Created basic layout.

---
+++ quasarapp/src/layouts/BottomNav.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-tabs slot="footer"
+          v-model="tab"
+          class="bg-primary text-white shadow-2"
+  >
+    <q-route-tab v-for="(nav, key) in navs" :icon="nav.icon" :to="nav.route" :key="key"></q-route-tab>
+  </q-tabs>
+</template>
+
+<script>
+
+export default {
+  name: 'BottomNav',
+  data() {
+    return {
+      navs: [
+        {
+          route: '/',
+          text: 'Home',
+          icon: 'home'
+        },
+        {
+          route: '/train',
+          text: 'Train',
+          icon: 'record_voice_over'
+        },
+        {
+          route: '/discover',
+          text: 'Discover',
+          icon: 'explore'
+        },
+        {
+          route: '/settings',
+          text: 'Settings',
+          icon: 'settings'
+        },
+      ]
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ quasarapp/src/pages/Train.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+
+</template>
+
+<script>
+export default {
+  name: 'Train'
+};
+</script>
+
+<style scoped>
+
+</style>
