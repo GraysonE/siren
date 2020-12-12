4483fcfe0698cc0bc966d41969e170e7be724c22
Grayson Erhard <web@graysonerhard.com>
2020-12-11 20:43:32 -0700

    stuffs playin

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,11 +1,97 @@
 <template>
   <q-page class="flex flex-center">
     <h2>Siren</h2>
+    <button @click="play">Play</button>
+    <button @click="stop">Stop</button>
   </q-page>
 </template>
 
 <script>
+var Tone = require('tone');
+import {Midi} from '@tonejs/midi';
+
+
 export default {
-  name: 'PageIndex'
-}
+  name: 'PageIndex',
+  data() {
+    return {
+      currentMidi: null,
+      playing: false,
+      mainLoop: null,
+      loops: [],
+      synths: [],
+      bpm: 240,
+      now: null,
+    }
+  },
+  mounted() {
+    this.load_midi();
+  },
+  methods: {
+    build() {
+
+      this.currentMidi.header.tempos.forEach((tempo) => {
+        tempo.bpm = this.bpm
+        this.currentMidi.header.update();
+      });
+      this.playing = true;
+      console.log(this.currentMidi);
+
+      if (this.playing && this.currentMidi) {
+        this.currentMidi.tracks.forEach((track) => {
+
+          console.log(track)
+
+          this.now = Tone.now() + 0.1;
+
+          //create a synth for each track
+          const synth = new Tone.PolySynth(Tone.Synth, {
+            envelope: {
+              attack: 0.02,
+              decay: 0.1,
+              sustain: 0.3,
+              release: 1,
+            },
+          }).toDestination();
+          this.synths.push(synth);
+
+          // this.mainLoop = new Tone.Loop(time => {
+            this.loop(track, synth);
+          // }, "1n").start(0);
+
+        });
+      } else {
+        //dispose the synth and make a new one
+        while (this.synths.length) {
+          const synth = this.synths.shift();
+          synth.dispose();
+        }
+      }
+
+
+    },
+    loop(track, synth) {
+      //schedule all of the events
+      track.notes.forEach((note) => {
+        synth.triggerAttackRelease(
+          note.name,
+          note.duration,
+          note.time + this.now,
+          note.velocity
+        );
+      });
+    },
+    play() {
+      this.build();
+      Tone.Transport.start();
+    },
+    stop() {
+      Tone.Transport.stop();
+    },
+    async load_midi() {
+      // load a midi file in the browser
+      this.currentMidi = await Midi.fromUrl("/midi/Octave.mid")
+    }
+  }
+};
 </script>
--- frontend/src/pages/Practice.vue
+++ frontend/src/pages/Practice.vue
@@ -1,36 +1,77 @@
 <template>
   <div>
-    <h3>asdf</h3>
-    <div class="wrap"></div>
+    <h3>Practice</h3>
+    <div class="wrap">
+      <button @click="play">Play</button>
+    </div>
   </div>
 </template>
 
 <script>
+
 var Tone = require('tone');
+import {Midi} from '@tonejs/midi'
 
-// create two monophonic synths
-const synthA = new Tone.FMSynth().toDestination();
-const synthB = new Tone.AMSynth().toDestination();
-
-// //play a note every quarter-note
-// const loopA = new Tone.Loop( time => {
-// 	synthA.triggerAttackRelease( 'C2', '8n', time );
-// }, '4n' ).start( 0 );
-//
-// //play another note every off quarter-note, by starting it "8n"
-// const loopB = new Tone.Loop( time => {
-// 	synthB.triggerAttackRelease( 'C4', '8n', time );
-// }, '4n' ).start( '8n' );
-//
-// // the loops start when the Transport is started
-// Tone.Transport.start();
-//
-// // ramp up to 800 bpm over 10 seconds
-// Tone.Transport.bpm.rampTo( 800, 10 );
+// const player = new Tone.Player("/assets/midi/Octave.mid").toDestination();
+// play as soon as the buffer is loaded
+// player.autostart = true;
+const sampler = new Tone.Sampler({
+  urls: {
+    //   A1: "A1.mp3",
+    //   A2: "A2.mp3",
+    //   Octave: "/assets/midi/Octave.mid"
+  },
+  baseUrl: "https://tonejs.github.io/audio/casio/",
+  onload: () => {
+    sampler.triggerAttackRelease(["C1", "E1", "G1", "B1"], 0.5);
+  }
+}).toDestination();
 
 
 export default {
-  name: 'Practice'
+  name: 'Practice',
+  data() {
+    return {}
+  },
+  mounted() {
+
+   this.load_midi();
+  },
+  methods: {
+    async play() {
+      console.log('hi')
+      await Tone.start()
+
+
+    },
+    async load_midi() {
+      // load a midi file in the browser
+      const midi = await Midi.fromUrl("/assets/midi/Octave.mid")
+//the file name decoded from the first track
+      const name = midi.name
+//get the tracks
+      midi.tracks.forEach(track => {
+        //tracks have notes and controlChanges
+
+        //notes are an array
+        const notes = track.notes
+        notes.forEach(note => {
+          //note.midi, note.time, note.duration, note.name
+        })
+
+        //the control changes are an object
+        //the keys are the CC number
+        track.controlChanges[64]
+        //they are also aliased to the CC number's common name (if it has one)
+        track.controlChanges.sustain.forEach(cc => {
+          // cc.ticks, cc.value, cc.time
+        })
+
+        //the track also has a channel and instrument
+        //track.instrument.name
+      })
+    }
+  }
 };
 </script>
 
